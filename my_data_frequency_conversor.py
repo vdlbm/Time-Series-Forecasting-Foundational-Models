adfc6e4684368c8cfb2ba141d505bb601d003550
Vic BM <vdelabarrera03@gmail.com>
2025-02-20 09:44:26 +0100

    Add data processing functions and initial model structure; update .gitignore

--- my_data_frequency_conversor.py
+++ my_data_frequency_conversor.py
@@ -6,23 +6,67 @@ import datetime as dt
 
 def mins_to_hours(df):
     """
-    Converts data with minute frequency to hourly frequency
+    Converts data with minute frequency to hourly frequency. Since the nyse stock market is open from 9:30 to 16:00, we're selecting
+    the data that includes minutes of 30 (9:30, 10:30, 11:30, 12:30, 13:30, 14:30 and 15:30), in our local time (14:30,...).
     """
 
-    # Separar los datos en columnas individuales
-    #df[['DateTime', 'Open', 'Close']] = df['DateTime;Open;Close'].str.split(';', expand=True)
+    # Establecer 'DateTime' como índice
+    df.set_index('DateTime', inplace=True)
 
-    # Convertir la columna 'DateTime' a tipo de datos de fecha y hora
-    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    # Seleccionar la columna 'Open' y convertirla en una Serie
+    s = df['Open']
+
+    # Filtrar la Serie para quedarse solo con los valores donde los minutos son 30
+    s_filtered = s[s.index.minute == 30]
+
+    s_filtered = s.filtered.asfreq('h')
+    return s_filtered
+
+def mins_to_days(df):
+    """
+    Converts data with minute frequency to hourly frequency. 
+    We´ll be selecting the open price of the stock at 9:30, 14:30 in our local time.
+    """
 
     # Establecer 'DateTime' como índice
-    #df.set_index('DateTime', inplace=True)
+    df.set_index('DateTime', inplace=True)
 
-    # Filtrar los datos para obtener solo los valores en los horarios deseados (por ejemplo, 9:30, 10:30, etc.)
-    filtered_df = df[df.index.strftime('%H:%M') == '09:30']
+    # Seleccionar la columna 'Open' y convertirla en una Serie
+    s = df['Open']
 
-    # Puedes agregar más horarios si lo deseas
-    horarios_deseados = ['14:30', '15:30', '16:30', '17:30', '18:30', '19:30', '20:30', '21:30', '22:30', '23:30']
-    filtered_df = df[df.index.strftime('%H:%M').isin(horarios_deseados)]
+    # Filtrar la Serie para quedarse solo con los valores donde los minutos son 30
+    s_filtered = s[(s.index.minute == 30) & (s.index.hour == 14)]
+
+    s_filtered = s.filtered.asfreq('d')
+
+    return s_filtered
+
+def mins_to_months(df):
+    """
+    Converts data with minute frequency to monthly frequency.
+    Selecting the last datapoint of each month.   
+    """
     
-    return filtered_df
+    # Establecer 'DateTime' como índice
+    df.set_index('DateTime', inplace=True)
+
+    # Seleccionar la columna 'Open' y convertirla en una Serie
+    s = df['Open']
+
+    # Agrupar los datos por mes y seleccionar el primer dato de cada mes
+    s_filtered = s.resample('ME').last()
+
+    s_filtered = s.filtered.asfreq('MS')
+
+    return s_filtered   
+
+def split_data(df):
+    """
+    Splits the data into train and validation sets.
+    """
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    df.set_index('DateTime', inplace=True)
+    train = df[df.index.year < 2024]
+    val = df[df.index.year == 2024]
+
+    return train, val
---
+++ my_models.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd 
+
+
+class Model(object):
+    def __init__(self, name):
+        self.name = name
